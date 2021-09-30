aa6a72ab25a5344b53a33e32df73e09bd9a8e02e
Oleg Belous <javelineua@gmail.com>
2021-09-30 17:46:04 +0300

    Fixed tables issue

--- flywayTesting/src/main/java/test1/flywayTesting/entities/Book.java
+++ flywayTesting/src/main/java/test1/flywayTesting/entities/Book.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -15,6 +17,7 @@ import javax.persistence.Id;
 public class Book {
 
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
     private String author;
     private String title;
